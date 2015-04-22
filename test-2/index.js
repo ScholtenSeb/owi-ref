259be0609e1e285a456f69a75bfabed5e1756fcc
ScholtenSeb <scholten.seb@gmail.com>
2015-04-22 23:04:16 +0200

    Restructure

--- test-2/index.js
+++ test-2/index.js
@@ -1,8 +1,31 @@
-var officegen = require("officegen-2");
+// OWI-REF APP
 
-var docx = officegen ( 'docx' );
+var express = require("express");
+var ejs = require("ejs");
+var bodyParser = require('body-parser');
+// var download = require('./docx.js');
+var morgan = require('morgan');
 
-var pObj = docx.createP();
-pObj.addText ( 'Simple' );
+var downloadWord = require('./modules/download');
 
-console.log(docx);
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get('/', function (req,res) {
+	res.render('index.html');
+});
+
+// app.post('/download', download.file);
+app.post('/coverpage', downloadWord.coverpage);
+app.post('/biblio', downloadWord.biblio);
+
+app.set('views', __dirname + '/views');  
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');
+
+app.listen(3000,function () {
+	console.log("listening on port 3000");
+})
